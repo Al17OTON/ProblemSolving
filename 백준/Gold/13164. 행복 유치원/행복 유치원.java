c76eb19b34f4ec51a4f4c7bd8a2442ab5249533f
limkwon <82102418+Al17OTON@users.noreply.github.com>
2024-12-09 22:08:56 +0900

    [Gold V] Title: 행복 유치원, Time: 476 ms, Memory: 64912 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -12,7 +12,6 @@ public class Main {
     // 가장 차가 많은 구간만 찾아서 빼주면 된다.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
@@ -22,12 +21,12 @@ public class Main {
         int pre = Integer.parseInt(st.nextToken());
         int tmp = pre;
         for(int n = 1; n < N; n++) {
-            int cur = Integer.parseInt(st.nextToken());
+            int cur = Integer.parseInt(st.nextToken());     //이전 원생의 키와 비교하여 차이만 기록한다.
             int distance = cur - pre;
             pq.offer(distance);
             pre = cur;
         }
-        result = pre - tmp;
+        result = pre - tmp;     //가장 작은 원생과 큰 원생의 차로 초기화
         
         // K - 1 만큼만 가장 큰 차이를 보이는 구간을 빼주면 된다.
         for(int k = 0; k < K - 1; k++) {
