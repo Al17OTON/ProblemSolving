f615e3a478b40195eab4f39e6188832790f7a748
limkwon <82102418+Al17OTON@users.noreply.github.com>
2024-12-11 15:36:48 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 13.63 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -10,6 +10,8 @@ class Solution {
         setTime(this.pos, pos);
         setTime(this.op_start, op_start);
         setTime(this.op_end, op_end);
+        
+        //시작 시 오프닝 체크
         checkOp();
         for(int i = 0; i < commands.length; i++) {
             if(commands[i].charAt(0) == 'p') {
@@ -17,14 +19,16 @@ class Solution {
             } else {
                 next();
             }
-            checkOp();
+            checkOp(); //매 순간 오프닝 체크
             // System.out.println(time2Str(this.pos));
         }
         
         return time2Str(this.pos);
     }
     
+    // 오프닝 여부 체크
     void checkOp() {
+        //시간 체크할때, 분이 이미 크다면 초는 계산하지 않아야 한다.
         if(op_start[0] <= pos[0] && (op_start[0] < pos[0] || op_start[1] <= pos[1]) && op_end[0] >= pos[0] && (op_end[0] > pos[0] || op_end[1] > pos[1])) {
             pos[0] = op_end[0];
             pos[1] = op_end[1];
@@ -66,12 +70,14 @@ class Solution {
         pos[1] = sec;
     }
     
+    // 문자열로 주어진 시간을 정수로 변환
     void setTime(int[] time, String str_time) {
         int idx = str_time.indexOf(':');
         time[0] = Integer.parseInt(str_time.substring(0, idx));
         time[1] = Integer.parseInt(str_time.substring(idx + 1));
     }
     
+    // 정수로 저장된 시간을 형식에 맞게 변환
     String time2Str(int[] time) {
         String min = String.format("%02d", time[0]);
         String sec = String.format("%02d", time[1]);
