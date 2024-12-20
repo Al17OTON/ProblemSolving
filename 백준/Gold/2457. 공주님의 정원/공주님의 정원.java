fbdb766930613170d01a0a3905a45e22dc1a688b
limkwon <82102418+Al17OTON@users.noreply.github.com>
2024-12-20 22:31:31 +0900

    [Gold III] Title: 공주님의 정원, Time: 424 ms, Memory: 51088 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -1,104 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Date implements Comparable<Date> {
-        int month, day;
-        public Date(int month, int day) {
-            this.month = month;
-            this.day = day;
-        }
 
-        @Override
-        public int compareTo(Date o) {
-            if(this.month < o.month || (this.month == o.month && this.day < o.day)) return -1;
-            else if(this.month == o.month && this.day == o.day) return 0;
-            return 1;
-        }
+    static class Flower {
+        int start_date, end_date;
 
-        @Override
-        public String toString() {
-            return "Date{" +
-                    "month=" + month +
-                    ", day=" + day +
-                    '}';
-        }
-
-        public int date2Idx() {
-            int idx = day;
-            for(int i = 1; i < month; i++) {
-                idx += months[i];
-            }
-            return idx;
+        public Flower(int start_date, int end_date) {
+            this.start_date = start_date;
+            this.end_date = end_date;
         }
     }
-    static class Flower implements Comparable<Flower> {
-        Date start, end;
+    
+    static class StartFlower implements Comparable<StartFlower> {
+        Flower f;
 
-        public Flower(Date start, Date end) {
-            this.start = start;
-            this.end = end;
+        public StartFlower(Flower f) {
+            this.f = f;
         }
 
         @Override
-        public int compareTo(Flower o) {
-            int res = this.start.compareTo(o.start);
-            if(res == 0) return this.end.compareTo(o.end);
-            return res;
-        }
-
-        @Override
-        public String toString() {
-            return "Flower{" +
-                    "start=" + start +
-                    ", end=" + end +
-                    '}';
+        public int compareTo(StartFlower o) {
+            return Integer.compare(this.f.start_date, o.f.start_date);
         }
     }
-    static int[] yearDate = new int[366];
-    static int[] months = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    static int N;
-    static List<Flower> flowers;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<StartFlower> start = new PriorityQueue<>();
         StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        flowers = new ArrayList<>();
         for(int n = 0; n < N; n++) {
             st = new StringTokenizer(br.readLine());
-            flowers.add(new Flower(new Date(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())), new Date(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()))));
+            int start_m = Integer.parseInt(st.nextToken());
+            int start_d = Integer.parseInt(st.nextToken());
+            int end_m = Integer.parseInt(st.nextToken());
+            int end_d = Integer.parseInt(st.nextToken());
+
+            start.offer(new StartFlower(new Flower(makeDate(start_m, start_d), makeDate(end_m, end_d))));
         }
 
-        Collections.sort(flowers);
-
-//        for(int i = 0; i < flowers.size(); i++) {
-//            System.out.println(flowers.get(i));
-//        }
-
-
-
-        System.out.println(dp());
-    }
-
-    static int dp() {
-        int curIdx = new Date(3,1).date2Idx();
-        int endPoint = new Date(12, 1).date2Idx();
-        for(int n = 0; n < flowers.size(); n++) {
-            Flower f = flowers.get(n);
-            int startIdx = f.start.date2Idx();
-            int endIdx = f.end.date2Idx();
-
-            if(startIdx > curIdx) return 0;
-            if(endIdx <= curIdx) continue;
-
-            for(; curIdx < endIdx; curIdx++) {
-                yearDate[curIdx] = yearDate[startIdx - 1] + 1;
+        // PriorityQueue<EndFlower> end = new PriorityQueue<>();
+        int current_time = 301, count = 0;
+        while(!start.isEmpty() && current_time < 1200) {
+            int max_end_time = -1;
+            while(!start.isEmpty() && start.peek().f.start_date <= current_time) {
+                if(isInside(start.peek().f, current_time)) max_end_time = Math.max(max_end_time, start.peek().f.end_date);
+                start.poll();
             }
 
-            if(endPoint <= curIdx) break;
+            if(max_end_time == -1) break;
+
+            count++;
+            current_time = max_end_time;
         }
-        return yearDate[endPoint - 1];
+
+        if(current_time < 1200) count = 0;
+
+        System.out.println(count);
+    }
+
+    static int makeDate(int m, int d) {
+        return m * 100 + d; 
+    }
+
+    static boolean isInside(Flower f, int date) {
+        return f.start_date <= date && f.end_date > date;
     }
 }
