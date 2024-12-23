7ef83bd407a5ebb98d48d3aca36040fcffb80d3b
limkwon <82102418+Al17OTON@users.noreply.github.com>
2024-12-23 22:40:33 +0900

    [Gold V] Title: 배, Time: 316 ms, Memory: 19796 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 우선순위 큐를 사용해서 해결하고자 하였는데 왜 메모리 초과가 나오는지 모르겠다...
 public class Main {
 
     static int N, M;
@@ -19,6 +20,7 @@ public class Main {
         for(int n = 0; n < N; n++) {
             crane[n] = Integer.parseInt(st.nextToken());
         }
+        // 오름차순 정렬
         Arrays.sort(crane);
 
         M = Integer.parseInt(br.readLine());
@@ -28,16 +30,20 @@ public class Main {
             box.add(Integer.parseInt(st.nextToken()));
         }
 
+        // 내림차순 정렬
         box.sort((a, b) -> b - a);
 
+        // 만약 크래인의 최대 운송 무게를 넘어가는 상자가 있다면 -1 출력
         if(box.get(0) > crane[N - 1]) {
             System.out.println(-1);
             return;
         }
+
+        // 각 크레인이 옮길 수 있는 가장 무거운 상자를 옮긴다.
         int time = 0;
         while(!box.isEmpty()) {
+            int j = 0;
             for(int i = N - 1; i >= 0; i--) {
-                int j = 0;
                 while(j < box.size() && box.get(j) > crane[i]) {
                     j++;
                 }
