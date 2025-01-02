b919772439aec92072a71c26b97d8e2b4946dff6
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-01-02 21:53:41 +0900

    [Gold V] Title: AC, Time: 676 ms, Memory: 96012 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -4,69 +4,47 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 
 public class Main {
-
-    static ArrayDeque<Integer> q = new ArrayDeque<>();
-    static boolean reverse;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
+        ArrayDeque<Integer> arr = new ArrayDeque<>();
         int T = Integer.parseInt(br.readLine());
-
         for(int t = 0; t < T; t++) {
-            reverse = false;
-            boolean error = false;
-            String com = br.readLine();
-            int N = Integer.parseInt(br.readLine());
-            parseQueue(br.readLine());
+            String p = br.readLine();
+            int n = Integer.parseInt(br.readLine());            
+            String tmp = br.readLine();
 
-            for(int i = 0; i < com.length() && !error; i++) {
-                char c = com.charAt(i);
-    
-                switch (c) {
-                    case 'R':
-                        reverse = !reverse;
+            if(tmp.length() > 2) {
+                for(String a : tmp.substring(1, tmp.length() - 1).split(",")) {
+                    arr.offer(Integer.parseInt(a));
+                }
+            } else arr.clear();
+            
+            boolean isReverse = false, isError = false;
+            for(int i = 0; i < p.length(); i++) {
+                char c = p.charAt(i);
+                if(c == 'R') isReverse = !isReverse;
+                else {
+                    if(arr.isEmpty()) {
+                        sb.append("error\n");
+                        isError = true;
                         break;
-                    case 'D':
-                        if(q.isEmpty()) {
-                            error = true;
-                            break;
-                        }
-                        if(!reverse) {
-                            q.poll();
-                        } else {
-                            q.removeLast();
-                        }
-                        break;
+                    }
+                    if(isReverse) arr.pollLast();
+                    else arr.pollFirst();
                 }
             }
-
-            if(error) System.out.println("error");
-            else {
-                StringBuilder sb = new StringBuilder();
-                sb.append("[");
-                while(!q.isEmpty()) {
-                    if(!reverse) sb.append(q.poll());
-                    else sb.append(q.removeLast());
-
-                    if(q.size() != 0) sb.append(",");
-                }
-                sb.append("]");
-
-                System.out.println(sb);
-            }
+            if(!isError) build(arr, isReverse, sb);
         }
+        System.out.println(sb);
     }
-
-    static void parseQueue(String a) {
-        q.clear();
-
-        if(a.length() == 2) return;
-
-        String[] list = a.substring(1, a.length() - 1).split(",");
-
-        for(String tmp : list) {
-            q.offer(Integer.parseInt(tmp));
+    
+    static void build(ArrayDeque<Integer> arr, boolean isReverse, StringBuilder sb) {
+        sb.append("[");
+        while(!arr.isEmpty()) {
+            sb.append(isReverse ? arr.pollLast() : arr.pollFirst());
+            if(!arr.isEmpty()) sb.append(",");
         }
+        sb.append("]\n");
     }
 }
