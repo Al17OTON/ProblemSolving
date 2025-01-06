2ad8c433355685fdde86829457d954b8720218af
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-01-06 23:08:50 +0900

    [Gold V] Title: 카드게임, Time: 144 ms, Memory: 29352 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10835. 카드게임/카드게임.java
+++ 백준/Gold/10835. 카드게임/카드게임.java
@@ -1,20 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
 // 오른쪽 카드만 버릴 수 있다면 무조건 버리기
 // 왼쪽 카드만 버리거나 왼오 둘다 버리는 것만 선택하면 됨
-// 가지치기?
+
 public class Main {
-    static int N, MAX = 0;
+    static int N;
     static int[] left, right;
+    static int[][] dp;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         left = new int[N];
         right = new int[N];
+        dp = new int[N][N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         for(int n = 0; n < N; n++) {
@@ -25,21 +28,21 @@ public class Main {
             right[n] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(0, 0, 0);
+        for(int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
 
-        System.out.println(MAX);
-        
+        System.out.println(dp(0, 0));
+
     }
 
-    static void dfs(int l, int r, int score) {
-        MAX = Math.max(MAX, score);
-        if(l == N || r == N) return;
+    static int dp(int l, int r) {
+        if(l == N || r == N) return 0;
 
-        if(left[l] > right[r]) {
-            dfs(l, r+1, score+right[r]);
-        } else {
-            dfs(l+1, r, score);
-            dfs(l+1, r+1, score);
-        }
+        if(dp[l][r] != -1) return dp[l][r];
+
+        if(left[l] > right[r]) return dp[l][r] = right[r] + dp(l, r + 1);
+        else return dp[l][r] = Math.max(dp(l + 1, r), dp(l + 1, r + 1));
+        
     }
 }
