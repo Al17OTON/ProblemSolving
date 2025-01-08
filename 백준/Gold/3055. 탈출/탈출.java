cfaef3674b24c8e7cab91a90dd7d056693de0b03
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-01-09 00:47:10 +0900

    [Gold IV] Title: 탈출, Time: 68 ms, Memory: 11864 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,110 +1,92 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    
-    static class Pos {
-        int r, c;
 
-        public Pos(int r, int c) {
+    static class Hadgehog {
+        int r, c;
+        public Hadgehog(int r, int c) {
             this.r = r;
             this.c = c;
         }
     }
-    static class Hedgehog extends Pos {
-        int len;
-
-        public Hedgehog(int r, int c, int len) {
-            super(r, c);
-            this.len = len;
-        }
-    }
-    /*
-1 4
-S.D*
-     */
 
     static int R, C;
-    static char[][] map;
-    static Queue<Hedgehog> water = new LinkedList<>();
-    static Hedgehog sonic;
-    static int[][] watermap;
-    
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
+    static char[][] map;
+    static Queue<Hadgehog> q = new LinkedList<>();
+    static Queue<Hadgehog> water = new LinkedList<>();
+    static Hadgehog goal;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new char[R][];
-        watermap = new int[R][C];
 
+        map = new char[R][];
         for(int r = 0; r < R; r++) {
             map[r] = br.readLine().toCharArray();
             for(int c = 0; c < C; c++) {
-                if(map[r][c] == '*') {
-                    watermap[r][c] = 1;
-                    water.offer(new Hedgehog(r, c, 1));
-                } else if(map[r][c] == 'S') {
+                if(map[r][c] == 'S') {
+                    q.add(new Hadgehog(r, c));
                     map[r][c] = '.';
-                    sonic = new Hedgehog(r, c, 0);
+                } else if(map[r][c] == '*') {
+                    water.add(new Hadgehog(r, c));
                 } else if(map[r][c] == 'D') {
-                    watermap[r][c] = Integer.MAX_VALUE;
+                    goal = new Hadgehog(r, c);
                 }
             }
         }
-
-        simWater();
-        int res = bfs();
-        System.out.println(res == -1 ? "KAKTUS" : res);
+        int result = sim();
+        System.out.println(result == -1 ? "KAKTUS" : result);
     }
 
-    static int bfs() {
+    static int sim() {
+        int time = 0;
         boolean[][] v = new boolean[R][C];
-        Queue<Hedgehog> son = new LinkedList<>();
-        son.offer(sonic);
-        v[sonic.r][sonic.c] = true;
+        v[q.peek().r][q.peek().c] = true;
 
-        while(!son.isEmpty()) {
-            Hedgehog s = son.poll();
-
-            if(map[s.r][s.c] == 'D') return s.len;
-
-            for(int i = 0; i < dr.length; i++) {
-                int nr = s.r + dr[i];
-                int nc = s.c + dc[i];
-
-                if(nr >= R || nr < 0 || nc >= C || nc < 0 || v[nr][nc] || map[nr][nc] == 'X' || (watermap[nr][nc] != 0 && watermap[nr][nc] <= s.len + 2)) continue;
-                v[nr][nc] = true;
-
-                son.offer(new Hedgehog(nr, nc, s.len + 1));
+        while(!q.isEmpty()) {
+            int waterSize = water.size();
+            for(int i = 0; i < waterSize; i++) {
+                Hadgehog w = water.poll();
+                for(int d = 0; d < 4; d++) {
+                    int nr = w.r + dr[d];
+                    int nc = w.c + dc[d];
+                    if(nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+                    if(map[nr][nc] == '.') {
+                        map[nr][nc] = '*';
+                        water.add(new Hadgehog(nr, nc));
+                    }
+                }
             }
 
+            int qSize = q.size();
+            for(int i = 0; i < qSize; i++) {
+                Hadgehog h = q.poll();
+                if(h.r == goal.r && h.c == goal.c) {
+                    return time;
+                }
+
+                for(int d = 0; d < 4; d++) {
+                    int nr = h.r + dr[d];
+                    int nc = h.c + dc[d];
+                    if(nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+                    if((map[nr][nc] == '.' || map[nr][nc] == 'D') && !v[nr][nc]) {
+                        v[nr][nc] = true;
+                        q.add(new Hadgehog(nr, nc));
+                    }
+                }
+            }
+            time++;
         }
 
         return -1;
-    }
-
-
-    static void simWater() {
-        while(!water.isEmpty()) {
-            Hedgehog w = water.poll();
-
-            for(int i = 0; i < dr.length; i++) {
-                int nr = w.r + dr[i];
-                int nc = w.c + dc[i];
-
-                if(nr >= R || nr < 0 || nc >= C || nc < 0 || map[nr][nc] != '.' || (watermap[nr][nc] != 0 && watermap[nr][nc] <= w.len + 1)) continue;
-
-                watermap[nr][nc] = w.len + 1;
-                water.offer(new Hedgehog(nr, nc, w.len + 1));
-            }
-        }
     }
 }
