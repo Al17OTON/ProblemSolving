1703715c3d144381815f9e07eb715f3640004e17
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-01-15 20:43:58 +0900

    [Gold V] Title: 사과나무, Time: 196 ms, Memory: 22376 KB -BaekjoonHub

---
+++ 백준/Gold/19539. 사과나무/사과나무.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] arr;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for(int n = 0; n < N; n++) {
+            arr[n] = Integer.parseInt(st.nextToken());
+            sum += arr[n];
+        }
+
+        boolean result = true;
+        if(sum % 3 != 0) result = false;
+        else {
+            result = treeSim(sum / 3);
+        }
+    
+        System.out.println(result ? "YES" : "NO");
+    }
+
+    static boolean treeSim(int three) {
+        int two = 0;
+        for(int n = 0; n < N; n++) {
+            two += arr[n] / 2;
+        }
+
+        return two >= three;
+    }
+}
