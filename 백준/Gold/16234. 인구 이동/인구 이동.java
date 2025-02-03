6fe600c74bfea6c2fc7b3e9483694a657f491b0b
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-02-03 17:41:12 +0900

    [Gold IV] Title: 인구 이동, Time: 452 ms, Memory: 294416 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,79 +1,79 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	
-	static boolean flag = true;
-	static Queue<Integer> q = new LinkedList<>();
-	static int[][] map;
-	static boolean[][] united;
-	static int N, L, R, sum;
-	static int[] dirX = {-1, 1, 0, 0};	//상하좌우
-	static int[] dirY = {0, 0, -1, 1};
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		L = sc.nextInt();
-		R = sc.nextInt();
-		
-		map = new int[N][N];
 
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
-		System.out.println(sim());
-	}
-	
-	static int sim() {
-		int result = 0;
-		int ii, jj;
-		while(flag) {
-			flag = false;				//국가간 인구 이동이 있었는지 체크
-			united = new boolean[N][N];	//매일 국경 초기화
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					sum = 0;
-					if(!united[i][j]) {
-						united[i][j] = true;
-						q.add(i); q.add(j);
-						sum += map[i][j];
-						checkUnite(i, j);
-						sum /= q.size() / 2;
-						while(!q.isEmpty()) {
-							ii = q.poll();
-							jj = q.poll();
-							
-							map[ii][jj] = sum;
-						}
-					}
-				}
-			}	
-			result++;    //인구 이동이 있었다면 날짜 세기
-		}
-		
-		return result - 1;
-	}
-	
-	// DFS로 연합이 가능한 모든 국가를 큐에 삽입한다.
-	static void checkUnite(int i, int j) {
-		int ii, jj;
-		for(int a = 0; a < dirX.length; a++) {
-			ii = i + dirX[a]; jj = j + dirY[a];
-			if(ii >= N || ii < 0 || jj >= N || jj < 0 || united[ii][jj]) continue;
-			int sub = Math.abs(map[ii][jj] - map[i][j]);
-			if(sub >= L && sub <= R) {
-				flag = true;			//한번이라도 인구 이동이 일어난다면 체크
-				q.add(ii); q.add(jj);
-				sum += map[ii][jj];
-				united[ii][jj] = true;
-				checkUnite(ii, jj);
-			}
-		}
-		
-	}
+    static int N, L, R;
+    static int[][] map;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+
+        for(int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for(int c = 0; c < N; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int day = 0;
+        while(true) {
+            boolean flag = false;
+            boolean[][] v = new boolean[N][N];
+            for(int r = 0; r < N; r++) {
+                for(int c = 0; c < N; c++) {
+                    if(!v[r][c]) flag |= floodFill(r, c, v);
+                }
+            }
+
+            if(!flag) break;
+            day++;
+        }
+
+        System.out.println(day);
+    }
+
+    static boolean floodFill(int r, int c, boolean[][] v) {
+        int sum = map[r][c];
+        v[r][c] = true;
+        Queue<int[]> q = new LinkedList(), unite = new LinkedList<>();
+        q.offer(new int[] {r, c});
+        unite.offer(new int[] {r, c});
+        
+        while(!q.isEmpty()) {
+            int[] p = q.poll();
+
+            for(int d = 0; d < 4; d++) {
+                int nr = p[0] + dr[d];
+                int nc = p[1] + dc[d];
+
+                if(nr >= N || nr < 0 || nc >= N || nc < 0 || v[nr][nc]) continue;
+                int sub = Math.abs(map[p[0]][p[1]] - map[nr][nc]);
+                if(sub >= L && sub <= R) {
+                    sum += map[nr][nc];
+                    v[nr][nc] = true;
+                    int[] next = new int[] {nr, nc};
+                    q.offer(next);
+                    unite.offer(next);
+                }
+            }
+        }
+        
+        if(unite.size() == 1) return false;
+
+        int pop = sum / unite.size();
+        while(!unite.isEmpty()) {
+            int[] p = unite.poll();
+            map[p[0]][p[1]] = pop;
+        }
+        return true;
+    }
 }
