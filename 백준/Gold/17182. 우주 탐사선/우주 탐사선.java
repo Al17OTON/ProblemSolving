2290a420301332edef7db9f5e5c68ea754cc2ad9
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-07-02 16:31:55 +0900

    [Gold III] Title: 우주 탐사선, Time: 92 ms, Memory: 11748 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.java
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, K;
     static int min = Integer.MAX_VALUE;
-    static int[][] adj;
     static int[][] dp;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,13 +13,11 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        adj = new int[N][N];
         dp = new int[N][N];
         for(int r = 0; r < N; ++r) {
             st = new StringTokenizer(br.readLine());
             for(int c = 0; c < N; ++c) {
-                adj[r][c] = Integer.parseInt(st.nextToken());
-                dp[r][c] = adj[r][c];
+                dp[r][c] = Integer.parseInt(st.nextToken());
             }
         }
 
