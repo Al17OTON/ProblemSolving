ae633defa3976c981e30414473001d7d07a867fa
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-11-13 16:36:59 +0900

    [level 2] Title: 튜플, Time: 20.68 ms, Memory: 5.86 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -4,42 +4,31 @@
 
 using namespace std;
 
-bool cmp(const vector<int>& a, const vector<int>& b) {
-    return a.size() < b.size();
-}
-
-bool v[100001] = {false};
+int counter[1000001] = {0};
 
 vector<int> solution(string s) {
     vector<int> answer;
     
-    vector<vector<int>> tuple;
-    int i = 2;
-    while(i < s.size() - 2) {
-        vector<int> tmp;
-        int num_s = i;
-        while(s[i] != '}') {
-            if(s[i] == ',') {
-                tmp.push_back(stoi(s.substr(num_s, i - num_s)));
-                num_s = i + 1;
+    bool flag = false;
+    int num_s = 0;
+    for(int i = 0; i < s.size(); ++i) {
+        if(s[i] < '0' || s[i] > '9') {
+            if(flag) {
+                flag = false;
+                ++counter[stoi(s.substr(num_s, i - num_s))];   
             }
-            ++i;
-        }
-        tmp.push_back(stoi(s.substr(num_s, i - num_s)));
-        tuple.push_back(tmp);
-        i += 3;   
-    }
-    
-    sort(tuple.begin(), tuple.end(), cmp);
-    
-    for(int i = 0; i < tuple.size(); ++i) {
-        for(int j = 0; j < tuple[i].size(); ++j) {
-            if(!v[tuple[i][j]]) {
-                answer.push_back(tuple[i][j]);
-                v[tuple[i][j]] = true;
-            }
+            num_s = i + 1;
+        } else {
+            flag = true;
         }
     }
-
+    
+    vector<pair<int,int>> tmp;
+    for(int i = 0; i < 1000001; ++i) {
+        if(counter[i] > 0) tmp.push_back({counter[i], i});
+    }
+    sort(tmp.rbegin(), tmp.rend());
+    for(int i = 0; i < tmp.size(); ++i) answer.push_back(tmp[i].second);
+    
     return answer;
 }
