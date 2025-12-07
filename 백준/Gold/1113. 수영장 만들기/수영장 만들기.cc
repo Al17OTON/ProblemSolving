339671e426d8eea91cdbc767729b134cb81e7193
limkwon <82102418+Al17OTON@users.noreply.github.com>
2025-12-08 03:08:23 +0900

    [Gold I] Title: 수영장 만들기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.cc
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.cc
@@ -1,75 +1,34 @@
 #include <iostream>
-#include <memory.h>
-#include <algorithm>
 #include <queue>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N, M;
 int map[52][52] = { 0 };
-bool v[52][52] = { false };
-bool vv[52][52] = { false };
-int dn[] = { -1, 1, 0, 0 };
-int dm[] = { 0, 0, -1, 1 };
+int dr[] = { -1, 1, 0, 0 };
+int dc[] = { 0, 0, -1, 1 };
 
-void floodfill() {
-	v[0][0] = true;
+void floodfill(int target) {
 	queue<pair<int, int>> q;
-	q.push({ 0,0 });
+
+	map[0][0] = target;
+	q.push({ 0, 0 });
 
 	while (!q.empty()) {
 		pair<int, int> p = q.front(); q.pop();
 
 		for (int d = 0; d < 4; ++d) {
-			int nn = p.first + dn[d];
-			int nm = p.second + dm[d];
+			int nr = p.first + dr[d];
+			int nc = p.second + dc[d];
 
-			if (nn > N + 1|| nn < 0 || nm > M + 1 || nm < 0 || v[nn][nm] || map[nn][nm] < map[p.first][p.second]) continue;
-			v[nn][nm] = true;
-			q.push({ nn, nm });
+			if (nr > N + 1 || nr < 0 || nc > M + 1 || nc < 0 || map[nr][nc] >= target) continue;
+			map[nr][nc] = target;
+			q.push({ nr,nc });
 		}
 	}
 
-
 }
-
-int same_low(int n, int m) {
-	vector<pair<int, int>> vec;
-	queue<pair<int, int>> q;
-	vv[n][m] = true;
-	q.push({ n, m });
-	vec.push_back({ n, m });
-
-	int low = 10;
-	while (!q.empty()) {
-		pair<int, int> p = q.front(); q.pop();
-
-		for (int d = 0; d < 4; ++d) {
-			int nn = p.first + dn[d];
-			int nm = p.second + dm[d];
-
-			if (map[n][m] < map[nn][nm] || v[nn][nm]) {
-				low = min(low, map[nn][nm]);
-				continue;
-			}
-			else if (vv[nn][nm]) continue;
-			vv[nn][nm] = true;
-			q.push({ nn, nm });
-			vec.push_back({ nn, nm });
-		}
-	}
-
-	int res = 0;
-	for (int i = 0; i < vec.size(); ++i) {
-		vv[vec[i].first][vec[i].second] = false;
-		res += max(0, low - map[vec[i].first][vec[i].second]);
-		map[vec[i].first][vec[i].second] = max(low, map[vec[i].first][vec[i].second]);
-	}
-	return res;
-}
-
-
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -77,23 +36,30 @@ int main() {
 
 	cin >> N >> M;
 
+	int high = 0;
 	for (int n = 1; n <= N; ++n) {
 		string line;
 		cin >> line;
 		for (int m = 1; m <= M; ++m) {
 			map[n][m] = line[m - 1] - '0';
+			high = max(high, map[n][m]);
 		}
 	}
 
-	floodfill();
-
 	int res = 0;
-	for (int n = 1; n <= N; ++n) {
-		for (int m = 1; m <= M; ++m) {
-			if (!v[n][m]) {
-				res += same_low(n, m);
+	for (int i = 1; i <= high; ++i) {
+		floodfill(i);
+
+		for (int r = 1; r <= N; ++r) {
+			for (int c = 1; c <= M; ++c) {
+				if (map[r][c] < i) {
+					++res;
+					++map[r][c];
+				}
 			}
 		}
 	}
+
 	cout << res;
 }
+
