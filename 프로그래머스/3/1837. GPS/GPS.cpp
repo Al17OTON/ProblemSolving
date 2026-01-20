d833a68f1b6c2118f205e5a31f074ec364209d5d
limkwon <82102418+Al17OTON@users.noreply.github.com>
2026-01-20 13:47:13 +0900

    [level 3] Title: GPS, Time: 7.36 ms, Memory: 5.25 MB -BaekjoonHub

--- 프로그래머스/3/1837. GPS/GPS.cpp
+++ 프로그래머스/3/1837. GPS/GPS.cpp
@@ -1,27 +1,16 @@
 #include <vector>
-#include <queue>
+#include <algorithm>
 #include <memory.h>
-#include <iostream>
 
 using namespace std;
 
-struct Taxi {
-    int idx;
-    int time;
-    int diff;
-    
-    bool operator < (const Taxi& o) const {
-        return this->diff > o.diff;
-    }
-};
-
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int n, int m, vector<vector<int>> edge_list, int k, vector<int> gps_log) {
-    int v[101][201];
-    vector<int> adj[201];
-    priority_queue<Taxi> pq;
+    int answer = 0;
     
-    fill(v[0], v[101], 987654321);
+    int dp[101][201];
+    fill(dp[0], dp[101], 987654321);
+    vector<int> adj[201];
     
     for(int i = 0; i < m; ++i) {
         int a = edge_list[i][0];
@@ -31,36 +20,19 @@ int solution(int n, int m, vector<vector<int>> edge_list, int k, vector<int> gps
         adj[b].push_back(a);
     }
     
-    pq.push({gps_log[0], 0, 0});
-    v[0][gps_log[0]] = 0;
+    dp[0][gps_log[0]] = 0;
     
-    while(!pq.empty()) {
-        Taxi taxi = pq.top(); pq.pop();
-        
-        if(taxi.time == k - 1) {
-            if(taxi.idx == gps_log.back()) return taxi.diff;
-            continue;
-        } 
-        
-        int log = gps_log[taxi.time + 1];
-        
-        for(int i = 0; i < adj[taxi.idx].size(); ++i) {
-            int next = adj[taxi.idx][i];
-            int diff = (log == next) ? taxi.diff : taxi.diff + 1;
+    for(int t = 0; t < k - 1; ++t) {
+        for(int j = 1; j <= n; ++j) {
+            if(dp[t][j] == 987654321) continue;
             
-            if(v[taxi.time + 1][next] > diff) {
-                v[taxi.time + 1][next] = diff;
-                pq.push({next, taxi.time + 1, diff});
+            for(int x = 0; x < adj[j].size(); ++x) {
+                int next = adj[j][x];
+                dp[t + 1][next] = min(dp[t + 1][next], dp[t][j] + (next == gps_log[t + 1] ? 0 : 1));
             }
+            dp[t + 1][j] = min(dp[t + 1][j], dp[t][j] + (j == gps_log[t + 1] ? 0 : 1));
         }
-        
-        pq.push({taxi.idx, taxi.time + 1, (log == taxi.idx ? taxi.diff : taxi.diff + 1)});
     }
     
-    return -1;
+    return (dp[k - 1][gps_log.back()] < 987654321 ? dp[k - 1][gps_log.back()] : -1);
 }
-
-
-
-
-
