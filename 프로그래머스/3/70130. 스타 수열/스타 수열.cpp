0666b499041a75d788db18c5f2ad70356ade303f
limkwon <82102418+Al17OTON@users.noreply.github.com>
2026-01-26 02:04:54 +0900

    [level 3] Title: 스타 수열, Time: 7.34 ms, Memory: 24.6 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.cpp
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.cpp
@@ -1,60 +1,28 @@
-#include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+#include <memory.h>
 
 using namespace std;
 
+int counter[500001], pos[500001];
+
 int solution(vector<int> a) {
-    vector<int> copy = a;
-    vector<pair<int, int>> mode;
-    sort(copy.begin(), copy.end());
+    memset(pos, -1, sizeof(pos));
     
-    while(!copy.empty()) {
-        int i = copy.back();
-        int count = 0;
-        while(!copy.empty() && copy.back() == i) {
-            copy.pop_back();
-            ++count;
+    for(int i = 0; i < a.size(); ++i) {
+        if(i - pos[a[i]] > 1) {
+            ++counter[a[i]];   
+        } else if(i + 1 < a.size() && a[i + 1] != a[i]) {
+            ++counter[a[i]];
+            pos[a[i]] = i + 1;
+            continue;
         }
-        mode.push_back({count, i});
+        pos[a[i]] = i;
     }
     
-    sort(mode.begin(), mode.end());
-    
-    int max_count = 0;
-    while(!mode.empty()) {
-        pair<int, int> p = mode.back();
-        mode.pop_back();
-        
-        if(max_count >= p.first) break;
-        
-        int count = 0;
-        int pre = -1, left_idx = -1;
-        for(int i = 0; i < a.size(); ++i) {
-            if(a[i] == p.second) {
-                // 왼쪽에 유효한 값이 있으면 우선 선택
-                if(left_idx != -1 && left_idx > pre) {
-                    pre = left_idx;
-                    ++count;
-                } else {
-                    int right_idx = i;
-                    while(++right_idx < a.size() && a[right_idx] == p.second);
-                    if(right_idx < a.size() && a[right_idx] != p.second) {
-                        pre = right_idx;
-                        ++count;
-                        i = pre;
-                    }
-                }
-                
-            } else {
-               left_idx = i; 
-            }
-        }
-        if(count == 0) return 0;
-        max_count = max(max_count, count);
+    int result = 0;
+    for(int i = 0; i < a.size(); ++i) {
+        result = max(result, counter[i]);
     }
-    
-    
-    return max_count * 2;
+    return result * 2;
 }
