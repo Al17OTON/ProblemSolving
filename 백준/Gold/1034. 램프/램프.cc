058d9d7bffa354dd22e30cfe7e6ef296ea8578d4
limkwon <82102418+Al17OTON@users.noreply.github.com>
2026-03-20 00:22:17 +0900

    [Gold IV] Title: 램프, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Gold/1034. 램프/램프.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, K;
+string desk[51];
+unordered_map<string, int> map;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+
+    for(int n = 0; n < N; ++n) {
+        string line;
+        cin >> line;
+        
+        desk[n] = line;
+        ++map[line];
+    }
+    cin >> K;
+
+    int max_result = 0;
+    for(int n = 0; n < N; ++n) {
+        int count = 0;
+        for(int m = 0; m < M; ++m) {
+            if(desk[n][m] == '0') ++count;
+        }
+
+        if(count <= K && count % 2 == K % 2) {
+            max_result = max(max_result, map[desk[n]]);
+        }
+    }
+
+    cout << max_result;
+}
