869e80032d3f1d0d85e129f2021a360e4ea8dde1
limkwon <82102418+Al17OTON@users.noreply.github.com>
2026-04-01 01:21:24 +0900

    [Gold V] Title: 머리 톡톡, Time: 120 ms, Memory: 6836 KB -BaekjoonHub

---
+++ 백준/Gold/1241. 머리 톡톡/머리 톡톡.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int N;
+vector<int> vec;
+vector<int> count(1000001, 0);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> N;
+
+    for(int n = 0; n < N; ++n) {
+        int num;
+        cin >> num;
+
+        vec.push_back(num);
+        ++count[num];
+    }
+
+    for(int n = 0; n < N; ++n) {
+        int root = sqrt(vec[n]);
+
+        int answer = 0;
+        for(int i = root; i >= 1; --i) {
+            if(vec[n] % i == 0) {
+                answer += count[i];
+                if(vec[n] != i * i) answer += count[vec[n] / i]; 
+            }
+        }
+
+        cout << answer - 1 << "\n";
+    }
+}
