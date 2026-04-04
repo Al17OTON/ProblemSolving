e097d49e2766ac7a121f7fc43a74fdd636c5121d
limkwon <82102418+Al17OTON@users.noreply.github.com>
2026-04-05 00:15:27 +0900

    [Gold V] Title: 무한 수열 2, Time: 8240 ms, Memory: 413072 KB -BaekjoonHub

--- 백준/Gold/1354. 무한 수열 2/무한 수열 2.cc
+++ 백준/Gold/1354. 무한 수열 2/무한 수열 2.cc
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <unordered_map>
+#include <map>
 
 using namespace std;
 
 long long N, P, Q, X, Y;
-unordered_map<long long, long long> map;
+map<long long, long long> m;
 
 long long dfs(long long i) {
     if(i <= 0) return 1;
-    if(map.find(i) != map.end()) return map.find(i)->second;
+    if(m.find(i) != m.end()) return m.find(i)->second;
 
-    return map[i] = dfs(i / P - X) + dfs(i / Q - Y);
+    return m[i] = dfs(i / P - X) + dfs(i / Q - Y);
 }
 
 int main() {
